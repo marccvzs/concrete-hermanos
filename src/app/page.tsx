9c5657835add888c6187c75f0ddd5aa2ba2dd98e
Marc Cavazos <marcavazos19@icloud.com>
2024-04-18 13:29:58 -0500

    actual db link to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
 import { db } from "~/server/db";
 
-const mockUrls = [
-  'https://utfs.io/f/eac7d4e4-5390-461a-81bc-c44cbe43077a-fd1vtg.webp',
-  'https://utfs.io/f/446f122f-73d0-4bd2-82ce-ebfeb62c23ae-1x3t6v.png',
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
+export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log('[+]: ', posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
-        {[... mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {[...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex flex-col">
             <img src={image.url} alt="image" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
