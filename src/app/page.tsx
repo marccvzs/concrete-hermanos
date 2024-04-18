ac4c0c155a1033541027062fbb6632f906acca26
Marc Cavazos <marcavazos19@icloud.com>
2024-04-18 14:04:40 -0500

    use auth for hp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
 import { db } from "~/server/db";
+import { SignedOut, SignedIn } from '@clerk/nextjs';
 
 export const dynamic = 'force-dynamic';
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48 flex flex-col">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default async function HomePage() {
+
+  return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 flex flex-col">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
