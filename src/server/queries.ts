da43f4b360a8443dd3c6603166f80ded23e5e851
Marc Cavazos <marcavazos19@icloud.com>
2024-04-19 16:04:37 -0500

    analytics on delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -5,6 +5,7 @@ import { images } from './db/schema';
 import { and, eq } from 'drizzle-orm';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import analyticsServerClient from './analytics';
 
 export async function getMyImages() {
     const user = auth();
@@ -43,5 +44,13 @@ export async function deleteImage(id: number) {
         .delete(images)
         .where(and(eq(images.id, id), eq(images.userId, user.userId)));
 
+    analyticsServerClient.capture({
+        distinctId: user.userId,
+        event: "delete image",
+        properties: {
+            imageId: id,
+        },
+    });
+    
     redirect('/');
 }
