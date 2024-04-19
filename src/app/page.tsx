3f4480d9d7e459c46b4645f0ebbb8a699cbcf4f4
Marc Cavazos <marcavazos19@icloud.com>
2024-04-18 21:51:19 -0500

    move to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[...images, ...images, ...images, ...images].map((image) => (
         <div key={image.id } className="w-48 h-48 flex flex-col">
           <Link href={`/img/${image.id}`}>
             <Image
