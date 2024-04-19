f14dccef8a06c482a7e6bf541d1a7b6a37548770
Marc Cavazos <marcavazos19@icloud.com>
2024-04-19 15:55:08 -0500

    add delete button

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,24 +1,39 @@
 import { clerkClient } from '@clerk/nextjs/server';
-import { getImage } from '~/server/queries';
+import { Button } from '~/components/ui/button';
+import { deleteImage, getImage } from '~/server/queries';
 
-export default async function FullPageImageView(props: { id: number }) {
-    const image = await getImage(props.id);
+export default async function FullPageImageView(props: { photoId: string }) {
+    const idAsNumber = Number(props.photoId);
+    if (Number.isNaN(idAsNumber)) throw new Error('Invalid photo id');
 
+    const image = await getImage(idAsNumber);
+ 
     const uploaderInfo = await clerkClient.users.getUser(image.userId);
     return (
-        <div className="flex w-full h-full m-w-0">
-            <div className="flex-shrink flex justify-center items-center">
-                <img src={image.url} className="flex-shrink object-contain" />
+        <div className="flex w-full h-full m-w-0 items-center justify-center text-white">
+            <div className="flex-shrink flex-grow">
+                <img src={image.url} className="object-contain" alt={image.name} />
             </div>
-            <div className="w-48 flex flex-shrink-0 flex-col border-l">
+            <div className="w-56 flex flex-shrink-0 flex-col border-1">
                 <div className="text-lg border-b text-center p-2">{image.name}</div>
-                <div className="flex flex-col p-2">
+                <div className="p-2">
                     <span>Uploaded By:</span>
                     <span>{uploaderInfo.fullName}</span>
                 </div>
-                <div className="flex flex-col p-2">
+                <div className="p-2">
                     <span>Created On:</span>
                     <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+                </div>
+                <div className="p-2">
+                    <form action={async () => {
+                        "use server";
+
+                        await deleteImage(idAsNumber);
+                    }}>
+                        <Button type="submit" variant="destructive">
+                            Delete
+                        </Button>
+                    </form>
                 </div>
             </div>
         </div>
